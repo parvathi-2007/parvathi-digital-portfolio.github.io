1200ccb82d3030fd2857890f24865bb212220d8e
parvathi-2007 <parvathikaruthedath2007@gmail.com>
2026-04-04 20:44:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function showMessage(){
+	alert("Welcome to my portfolio!!")
+}
+
+function validateForm(){
+	let name =document.getElementById("name").value.trim();
+	let email =document.getElementById("email").value.trim();
+ 	let message =document.getElementById("message").value.trim();
+	let error =document.getElementById("error");
+
+if(name===""||email===""||message===""){
+	error.textContent= "All fields are required";
+	return false;
+}
+if(!email.includes("@")||!email.includes(".")||!email.includes("gmail.com")){
+	error.textContent= "Enter a Valid Email";
+	return false;
+}
+error.tetxtContent= " ";
+alert ("Form submitted Successfully!!");
+return true;
+}
+
